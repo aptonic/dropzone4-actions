3d031cc5c472962fcff3d7ace1594f812d128ddc
aptonic <contact@aptonic.com>
2014-03-13 21:24:29 +1300

    Added Desktop Picture action

---
+++ Desktop Picture.dzbundle/action.rb
@@ -0,0 +1,31 @@
+# Dropzone Action Info
+# Name: Desktop Picture
+# Description: A dropped image will be set as the current desktop background.
+# Handles: Files
+# Events: Dragged
+# Creator: Aptonic Software
+# URL: http://aptonic.com
+# Version: 1.0
+# RunsSandboxed: No
+# MinDropzoneVersion: 3.0
+
+def dragged
+  $dz.determinate(false)
+  
+  file_path = $items[0]
+  file_name = File.basename(file_path)
+  
+  $dz.begin("Changing to #{file_name}...")
+  
+  allowed_exts = ["jpg", "jpeg", "gif", "tif", "tiff", "png", "bmp"]
+  ext = File.extname(file_path).downcase[1..-1]
+  
+  if allowed_exts.include?(ext)
+    %x(osascript -e 'tell application "System Events" to set picture of current desktop to "#{file_path}"' >& /dev/null)
+    $dz.finish("Desktop Changed")
+  else
+    $dz.finish("Not an Image")
+  end
+  
+  $dz.url(false)
+end
