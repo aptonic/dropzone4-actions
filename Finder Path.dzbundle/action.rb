b440959fd931e205cbb372e50e0977d6b9efd5b1
aptonic <contact@aptonic.com>
2014-03-28 13:45:01 +1300

    Changed Finder Path RunsSandboxed to No

--- Finder Path.dzbundle/action.rb
+++ Finder Path.dzbundle/action.rb
@@ -1,12 +1,11 @@
 # Dropzone Action Info
 # Name: Finder Path
 # Description: Copies the path of the currently selected item in the Finder to the clipboard.
-# Handles: Files
 # Events: Clicked
 # Creator: Aptonic Software
 # URL: http://aptonic.com
 # Version: 1.0
-# RunsSandboxed: Yes
+# RunsSandboxed: No
 # MinDropzoneVersion: 3.0
 
 def clicked
@@ -16,7 +15,7 @@ tell application "Finder"
 	try
 		set theSelection to (target of window 1 as alias)
 		set theSelection to theSelection as string
-		set finder_path to POSIX path of theSelection
+		set finder_path to POSIX path of (the selection as alias)
 	on error
 		set finder_path to null
 	end try
