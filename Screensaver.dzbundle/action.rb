39455a17cf0a754bd1fc5d2714632e1a3206bc3a
aptonic <contact@aptonic.com>
2014-06-02 17:35:43 +1200

    Testing Update system

--- Screensaver.dzbundle/action.rb
+++ Screensaver.dzbundle/action.rb
@@ -4,13 +4,13 @@
 # Events: Clicked
 # Creator: Aptonic Software
 # URL: http://aptonic.com
-# Version: 1.1
+# Version: 1.2
 # RunsSandboxed: No
 # MinDropzoneVersion: 3.0
 # UniqueID: 1006
  
 def clicked
-  $dz.finis("test")
+  $dz.finish("test")
   $dz.url(false)
   `osascript -e 'tell application "System Events" to start current screen saver' >& /dev/null`
 end
