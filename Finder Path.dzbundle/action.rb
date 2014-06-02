20fd4bccebf2c8752ec32ef9fccb59db4b843e6e
aptonic <contact@aptonic.com>
2014-06-02 15:38:17 +1200

    Testing Update system

--- Finder Path.dzbundle/action.rb
+++ Finder Path.dzbundle/action.rb
@@ -4,7 +4,7 @@
 # Events: Clicked
 # Creator: Aptonic Software
 # URL: http://aptonic.com
-# Version: 1.2
+# Version: 1.3
 # SkipConfig: Yes
 # RunsSandboxed: No
 # MinDropzoneVersion: 3.0
@@ -26,7 +26,7 @@ if finder_path is not null then return finder_path
 END`
 
   if (path == nil or path == "")
-    $dz.finish("Cool Update")
+    $dz.finish("Cool Update again")
     $dz.url(false)
   else 
     $dz.finish("Path copied")
