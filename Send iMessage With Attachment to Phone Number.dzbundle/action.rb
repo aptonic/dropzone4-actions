f7afcf532467265a1baadbfc6408a6e72becf580
aptonic <contact@aptonic.com>
2014-08-27 16:28:21 +1200

    Fixed whitespace issue in last commit

--- Send iMessage With Attachment to Phone Number.dzbundle/action.rb
+++ Send iMessage With Attachment to Phone Number.dzbundle/action.rb
@@ -32,14 +32,14 @@ tell application "Messages"
   set targetService to 1st service whose service type = iMessage
   set targetBuddy to buddy "#{phone_number}" of targetService
   send theAttachment1 to targetBuddy
-	try
-		close window "File Transfers"
-	end try
+  try
+    close window "File Transfers"
+  end try
 end tell
 tell application "Finder"
-	try
-		set visible of process "Messages" to false
-	end try
+  try
+    set visible of process "Messages" to false
+  end try
 end tell
 END`
   puts result
