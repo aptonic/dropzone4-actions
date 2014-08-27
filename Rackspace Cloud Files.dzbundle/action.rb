9b883570809def514dbea40bd6733d9c2439f844
aptonic <contact@aptonic.com>
2014-08-28 01:14:25 +1200

    Set RunsSandboxed: Yes for Rackspace Cloud Files action

--- Rackspace Cloud Files.dzbundle/action.rb
+++ Rackspace Cloud Files.dzbundle/action.rb
@@ -7,8 +7,8 @@
 # OptionsNIB: UsernameAPIKey
 # Events: Dragged, Clicked
 # SkipConfig: No
-# RunsSandboxed: No
-# Version: 1.5
+# RunsSandboxed: Yes
+# Version: 1.6
 # MinDropzoneVersion: 3.0
 # UniqueID: 1017
 
