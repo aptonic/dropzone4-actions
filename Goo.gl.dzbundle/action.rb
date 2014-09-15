22e0ac90bb4d97f3c99c8f465960059efa888404
Alexandru Chirițescu <alex@alexchiri.com>
2014-09-15 21:51:28 +0200

    Goo.gl action
    * action now works also if authorization is not completed
    * updated description to inform that the authorization is optional
    * added message for the case when the supplied URL is http://goo.gl/ or https://goo.gl/
    * increased version to 1.3

--- Goo.gl.dzbundle/action.rb
+++ Goo.gl.dzbundle/action.rb
@@ -1,6 +1,6 @@
 # Dropzone Action Info
 # Name: Goo.gl
-# Description: Dropped URLs will be converted to short Goo.gl URLs and short Goo.gl URLs will be expanded. When clicked, the action will attempt to do the same with any URLs present in clipboard.
+# Description: Dropped URLs will be converted to short Goo.gl URLs and short Goo.gl URLs will be expanded. When clicked, the action will attempt to do the same with any URLs present in clipboard. \n\nAuthorization is optional, use it only if you want to track the shortened URLs using your Google account.
 # Handles: Text
 # Creator: Dominique Da Silva
 # URL: http://www.agonia.fr
@@ -9,7 +9,7 @@
 # AuthScope: https://www.googleapis.com/auth/urlshortener
 # SkipValidation: Yes
 # RunsSandboxed: No
-# Version: 1.2
+# Version: 1.3
 # MinDropzoneVersion: 3.2.1
 # UniqueID: 1024
 
