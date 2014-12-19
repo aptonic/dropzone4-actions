cb902cd85469bbaac4c511d83c734196ae52e2e1
Dennis Kuypers <dev@kymedia.de>
2014-12-19 23:35:54 +0100

    Initial Terminal commit
    Opens a new terminal window using osascript. The file or path dropped
    will be the location for the terminal to start in.

---
+++ Terminal.dzbundle/action.rb
@@ -0,0 +1,48 @@
+# Dropzone Action Info
+# Name: Terminal
+# Description: Opens a Terminal in the path that is dropped. If a file is given the containing directory will be used. If multiple files are given only the first one is considered
+# Handles: Files, Text
+# Creator: Dennis Kuypers
+# URL: http://dk.kymedia.de
+# Events: Clicked, Dragged
+# KeyModifiers:
+# SkipConfig: No
+# RunsSandboxed: Yes
+# Version: 1.0
+# MinDropzoneVersion: 3.0
+
+def dragged
+    
+    # place to store the desired directory
+    dir = false
+    
+    # parse the input
+    case ENV['dragged_type']
+        when 'files'
+        # Get first file and launch in this directory
+        dir = File.dirname($items[0])
+        when 'text'
+        # Verify that this is a directory path
+        dir = $items[0] if File.directory?(dir)
+    end
+    
+    # Launch Terminal in desired directory
+    if dir
+        system("osascript -e 'tell application \"Terminal\" to do script \"cd #{dir};clear\"'")
+        else
+        # Could not figure out what the user wants. Dump to console and notify user.
+        puts "Could not figure out what to do with data:"
+        puts $items.inspect
+        $dz.fail("This is not a file or directory path.")
+    end
+    
+    # nothing to do here...
+    $dz.url(false)
+end
+
+def clicked
+    # FIXME there is probably a way to launch a new window without the "do script" part?
+    puts system("osascript -e 'tell application \"Terminal\" to do script \"\"'")
+    # nothing to do here...
+    $dz.url(false)
+end
