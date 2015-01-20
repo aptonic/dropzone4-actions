14c355a347309835919193c6502a6376a30a404f
Dominique Da Silva <dom@agonia.fr>
2015-01-20 17:23:02 +0100

    Added Boxcar action

---
+++ Boxcar.dzbundle/action.rb
@@ -0,0 +1,43 @@
+# Dropzone Action Info
+# Name: Boxcar
+# Description: Send message to Boxcar App (https://boxcar.io/).
+# Creator: Dominique Da Silva
+# URL: http://www.agonia.fr
+# Events: Clicked, Dragged
+# SkipConfig: No
+# RunsSandboxed: Yes
+# Version: 1.0
+# MinDropzoneVersion: 3.0
+# Handles: Text
+# OptionsNIB: APIKey
+# LoginTitle: Boxcar User API Key
+
+require 'notification'
+
+def send(data)
+
+	$dz.begin("Sending message via Boxcar")
+	
+	notification = Notification.new('boxcar', ENV['api_key'])
+	notification.message = data.chomp
+	notification.sound = 'beep-soft'
+
+	notification.push
+
+end
+
+def readClipboard
+	IO.popen('pbpaste') {|clipboard| clipboard.read}
+end
+
+def dragged
+	if ENV['dragged_type'] == 'text'
+		send($items[0])
+		$dz.finish("Message sent via Boxcar")
+	end
+	$dz.url(false)
+end
+
+def clicked
+	system("open https://new.boxcar.io/account")
+end
