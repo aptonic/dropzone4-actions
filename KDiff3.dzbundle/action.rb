6b8b67ff1bd318a4f2e3b265cc9c67d4c34ca482
Eric W. Wallace <e@ewall.org>
2015-02-13 23:38:35 -0500

    KDiff3: now only prompts for app path if not found at default

--- KDiff3.dzbundle/action.rb
+++ KDiff3.dzbundle/action.rb
@@ -1,60 +1,70 @@
 # Dropzone Action Info
 # Name: KDiff3
-# Description: Drag 2 or 3 files or folders to diff them with KDiff3. Hold Command key (⌘) to merge the items.
+# Description: Drag 2 or 3 files or folders to diff them with KDiff3. Hold a modifier key (⌘, ⌥, ⌃, or ⇧) while dragging to merge the items.
 # Creator: Eric W. Wallace
-# URL: http://www.ewall.org/
+# URL: http://www.ewall.org/dev/dropzone
 # Handles: Files
 # Events: Clicked, Dragged
-# KeyModifiers: Command
+# KeyModifiers: Command, Option, Control, Shift
 # SkipConfig: Yes
-# OptionsNIB: ChooseApplication
 # RunsSandboxed: Yes
+# UniqueID: 1033
 # Version: 1.0
 # MinDropzoneVersion: 3.0
 
 def findApp
-  if ENV["path"] == ""
-    # not configured, so let's try the default location
-    app = '/Applications/kdiff3.app/Contents/MacOS/kdiff3'
-  else
-    # is configured, build path to actual executable
-    app = ENV["path"] + '/Contents/MacOS/kdiff3'
-  end
+	# try default location to start
+	app = '/Applications/kdiff3.app/Contents/MacOS/kdiff3'
 
-  unless FileTest.executable?(app)
-    $dz.fail('Cannot open KDiff3. Please confirm it is installed and configure the package with the correct location.')
-    exit
-  end
+	unless FileTest.executable?(app)
 
-  return app
+		if !ENV['apppath'].nil? and FileTest.executable?(ENV['apppath'])
+			# we have a valid saved path
+			app = ENV['apppath']
+
+		else
+			# no saved path, so prompt for it
+			output = $dz.cocoa_dialog('fileselect --title "Select the Application" --informative-text "Browse to the KDiff3 app:" --with-directory /Applications --select-directories ‑‑with‑extensions .app')
+			apppkg = output.strip
+
+			$dz.fail("Cancelled") if apppkg == ''
+
+			app = "#{apppkg}/Contents/MacOS/kdiff3"
+			unless FileTest.executable?(app)
+				$dz.fail('Cannot find KDiff3 at given location; please try again and browse to the appropriate app.')
+			end
+
+			# ...and save it for future use
+			$dz.save_value('apppath', app)
+		end
+
+	end
+	return app
 end
 
 def dragged
-  $dz.begin("Opening KDiff...")
+	$dz.begin("Opening KDiff3...")
 
-  if $items.size < 2
-    $dz.fail "We can compare only 2 or 3 items. Have you considered stashing files or folders in the Drop Bar, then dragging the items as group?"
-    exit
-  elsif $items.size > 3
-    $dz.finish "Sorry, but we can compare no more than 3 items (files or directories)."
-  end
+	if $items.size < 2
+		$dz.fail "KDiff3 can compare 2 or 3 items, whether files or folders. Have you considered stashing the itmes in the Drop Bar, then dragging them onto the action as group?"
+		exit
+	elsif $items.size > 3
+		$dz.finish "Sorry, but KDiff3 can only compare no more than 3 items (files or folders)."
+	end
 
-  if ENV["KEY_MODIFIERS"] == "Command"
-    $items.push('-m') # add flag to merge
-  end
+	if !ENV['KEY_MODIFIERS'].nil?
+		$items.push('-m') # add flag to merge
+	end
 
-  unless system(findApp(), *$items)
-    $dz.fail('Error opening KDiff3')
-  end
-
-  $dz.url(false)
+	result = system(findApp(), *$items)
+	#$dz.fail("Error executing KDiff3") if result > 0 #skip error check due to Mavericks' "modalSession has been exited prematurely" bug on KDiff3 and other apps
+	$dz.url(false)
 end
 
 def clicked
-  $dz.begin("Opening KDiff...")
+	$dz.begin("Opening KDiff3...")
 
-  unless system(findApp(), *$items)
-    $dz.fail('Error opening KDiff3')
-  end
-  $dz.url(false)
+	result = system(findApp())
+	#$dz.fail("Error executing KDiff3") if result > 0  #skip error check due to Mavericks' "modalSession has been exited prematurely" bug on KDiff3 and other apps
+	$dz.url(false)
 end
