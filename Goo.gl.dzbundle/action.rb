876a4deede3237ee2806f74c180d494adf38c5bd
aptonic <contact@aptonic.com>
2015-07-06 16:24:54 +1200

    Made Goo.gl action correct URLs pasted from Chrome location bar that don't have http:// at the start

--- Goo.gl.dzbundle/action.rb
+++ Goo.gl.dzbundle/action.rb
@@ -1,6 +1,6 @@
 # Dropzone Action Info
 # Name: Goo.gl
-# Description: Dropped URLs will be converted to short Goo.gl URLs and short Goo.gl URLs will be expanded. When clicked, the action will attempt to do the same with any URLs present in clipboard. \n\nAuthorization is optional, use it only if you want to track the shortened URLs using your Google account.
+# Description: Dropped URLs will be converted to short Goo.gl URLs and short Goo.gl URLs will be expanded. When clicked, the action will attempt to do the same with any URLs present in clipboard.\n\nAuthorization is optional, use it only if you want to track the shortened URLs using your Google account.
 # Handles: Text
 # Creator: Dominique Da Silva
 # URL: http://www.agonia.fr
@@ -9,7 +9,7 @@
 # AuthScope: https://www.googleapis.com/auth/urlshortener
 # SkipValidation: Yes
 # RunsSandboxed: No
-# Version: 1.4
+# Version: 1.5
 # MinDropzoneVersion: 3.2.1
 # UniqueID: 1024
 # RubyPath: /System/Library/Frameworks/Ruby.framework/Versions/2.0/usr/bin/ruby
