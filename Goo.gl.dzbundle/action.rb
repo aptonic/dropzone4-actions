d9ec31fef7ee3e26bf35f254c98e8ed28fe98902
aptonic <contact@aptonic.com>
2015-07-07 20:54:10 +1200

    More Goo.gl action improvements

--- Goo.gl.dzbundle/action.rb
+++ Goo.gl.dzbundle/action.rb
@@ -1,6 +1,6 @@
 # Dropzone Action Info
 # Name: Goo.gl
-# Description: A dropped URL will be converted to a shortened Goo.gl URL.\nYou can also drop shortened URLs to get the expanded URL.\n\nWhen clicked, the action will attempt to shorten or expand a URL on the clipboard.\n\nAuthorization is optional, use it only if you want to track the shortened URLs using your Google account.
+# Description: A dropped URL will be converted to a shortened Goo.gl URL.\nYou can also drop shortened URLs to get the expanded URL.\n\nWhen clicked, the action will attempt to shorten or expand a URL on the clipboard.\n\nAuthorization is optional, use it if you want to track the shortened URLs at http://goo.gl using your Google account.
 # Handles: Text
 # Creator: Dominique Da Silva
 # URL: http://www.agonia.fr
@@ -9,7 +9,7 @@
 # AuthScope: https://www.googleapis.com/auth/urlshortener
 # SkipValidation: Yes
 # RunsSandboxed: Yes
-# Version: 1.7
+# Version: 1.8
 # MinDropzoneVersion: 3.2.3
 # UniqueID: 1024
 # RubyPath: /System/Library/Frameworks/Ruby.framework/Versions/2.0/usr/bin/ruby
@@ -17,6 +17,8 @@
 require 'googl'
 
 def dragged
+  $dz.determinate(false)
+  $dz.begin('Getting Goo.gl URL')
   googl = Googl.new
 
   googl.configure_client
@@ -24,6 +26,8 @@ def dragged
 end
 
 def clicked
+  $dz.determinate(false)
+  $dz.begin('Getting Goo.gl URL')
   googl = Googl.new
 
   googl.configure_client
