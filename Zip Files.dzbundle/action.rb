a3ebd38aca980ebca39c154cd24917c3adb55303
aptonic <contact@aptonic.com>
2018-06-24 12:11:20 +1200

    Zip Files action now uses folder name as zip file name if just dropping one folder

--- Zip Files.dzbundle/action.rb
+++ Zip Files.dzbundle/action.rb
@@ -8,13 +8,21 @@
 # Events: Dragged
 # SkipConfig: No
 # RunsSandboxed: No
-# Version: 1.2
+# Version: 1.3
 # MinDropzoneVersion: 3.2.1
 # UniqueID: 1025
 
 def dragged
+  
   $dz.determinate(false)
-  filename = $dz.inputbox("New ZIP File", "Enter name for new zip file (minus extension):")
+  
+  # Use folder name as the zip file name if just one folder is dropped
+  if $items.length == 1 and File.directory?($items[0])
+    filename = $items[0].split(File::SEPARATOR).last
+  else
+    filename = $dz.inputbox("New ZIP File", "Enter name for new zip file (minus extension):")  
+  end
+  
   zipfile = ZipFiles.zip($items, filename + ".zip")
   zipfile = zipfile[1, zipfile.length - 2]
   system("zip -d \"#{zipfile}\" '__MACOSX*' '*.DS_Store' >& /dev/null")
