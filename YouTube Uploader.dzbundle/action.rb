69759c586f5d1c51eaa6fdabdef2bd0bea00e0f9
aptonic <contact@aptonic.com>
2020-07-01 13:05:19 +1200

    Update YouTube Uploader icon

--- YouTube Uploader.dzbundle/action.rb
+++ YouTube Uploader.dzbundle/action.rb
@@ -8,7 +8,7 @@
 # AuthScope: https://www.googleapis.com/auth/youtube.upload
 # Events: Dragged, Clicked
 # RunsSandboxed: Yes
-# Version: 2.3
+# Version: 2.4
 # UniqueID: 1026
 # MinDropzoneVersion: 3.2.1
 
