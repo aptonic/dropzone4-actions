9735bc2c411690d14b1c6d8012b55966641f3035
aptonic <contact@aptonic.com>
2021-01-08 18:34:33 +1300

    Update Send iMessage action to use Pashua

--- Send iMessage With Attachment to Phone Number.dzbundle/action.rb
+++ Send iMessage With Attachment to Phone Number.dzbundle/action.rb
@@ -1,13 +1,13 @@
 # Dropzone Action Info
 # Name: Send iMessage With Attachment to Phone Number
-# Description: Send an iMessage with a file attachment to a preset phone number.\n\nClick the action in the grid to change the phone number.
+# Description: Send an iMessage with a file attachment to a preset phone number.\n\nClick the action in the grid to set the phone number.
 # Handles: Files
 # Creator: Aptonic Software
 # URL: http://aptonic.com
 # Events: Clicked, Dragged
 # SkipConfig: No
 # RunsSandboxed: No
-# Version: 1.4
+# Version: 1.5
 # MinDropzoneVersion: 3.0
 # UniqueID: 1021
 
@@ -47,6 +47,8 @@ tell application "Finder"
 end tell
 END`
   puts result
+
+  $dz.error("Failed to send iMessage", "Failed to send to contact. Try opening iMessage and sending a message to this number manually first, then try again with Dropzone.") if result =~ /Can’t get buddy id/
   $dz.error("Failed to send iMessage", "Failed to send iMessage to the entered phone number.\n\n#{result}") if result =~ /error/
   end
   
@@ -61,14 +63,20 @@ def clicked
 end
 
 def read_phone_number
-  # Get the phone number to send to
-  output = $dz.cocoa_dialog('inputbox --button1 "OK" --button2 "Cancel" --title "Phone Number" --e --informative-text "Enter the phone number to send to (this will be saved so you only have to enter it once):"')
-
-  button, phone_number = output.split("\n")
-
-  if button == '2'
+  config = "
+  *.title = Phone Number
+  p.type = textfield
+  p.width = 500
+  p.label = Enter the phone number to send to (this will be saved so you only have to enter it once):
+  cb.type = cancelbutton
+  "
+  result = $dz.pashua(config)
+  
+  if result['cb'] == '1'
     $dz.fail('Cancelled')
   end
+
+  phone_number = result['p']
 
   # Fail if no phone number is entered
   if phone_number.to_s.strip.length == 0
