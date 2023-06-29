e1d0bf4e1c0d94938d354d0bbf6b92fc0f033c93
aptonic <contact@aptonic.com>
2023-06-29 17:11:02 +1200

    Testing bundles updater

--- Tinify.dzbundle/action.py
+++ Tinify.dzbundle/action.py
@@ -20,7 +20,7 @@ import tinify
 def dragged():
     tinify.key = os.environ["api_key"]
     upload_and_compress_images(items)
-    print("Testing 123")
+    print("Testing 123456")
 
 def upload_and_compress_images(paths):
     if not check_img_types_valid(paths):
