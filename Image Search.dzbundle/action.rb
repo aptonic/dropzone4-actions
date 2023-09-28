f41c6e3525bb27c31a4e7d9e0bc12a2102f99103
aptonic <contact@aptonic.com>
2023-09-28 23:39:23 +1300

    Fix Image Search issue

--- Image Search.dzbundle/action.rb
+++ Image Search.dzbundle/action.rb
@@ -7,7 +7,7 @@
 # Events: Dragged, Clicked
 # SkipConfig: No
 # RunsSandboxed: Yes
-# Version: 1.1
+# Version: 1.2
 # MinDropzoneVersion: 3.0
 # UniqueID: 1022
 
@@ -35,7 +35,21 @@ def dragged
 
   results.each do |result|
     check_upload_output_valid(result)
-    extracted_url = /URL=(.*)\"/.match(result[:output])[1]
+
+    match_data = /URL=(.*)\"/.match(result[:output])
+
+    if match_data
+      extracted_url = match_data[1]
+    else
+      # Try a different match
+      match_data = /url=(.*)\"/.match(result[:output])
+      if match_data
+        extracted_url = "https://lens.google.com" + match_data[1]
+      else
+        $dz.error("Error searching for image", "Uploader output:\n\n#{result}")
+      end
+    end
+
     system("open \"#{extracted_url}\"")
   end
   
@@ -45,7 +59,7 @@ end
 
 def check_upload_output_valid(result)
   final_result = result[:output]
-  if result[:curl_output_valid] and final_result =~ /URL=/
+  if result[:curl_output_valid] and final_result =~ /URL=/ or final_result =~ /url=/
     return true
   else
     error_message = final_result
