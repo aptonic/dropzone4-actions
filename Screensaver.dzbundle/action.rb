aea25565bdf674796908cc11fe4e1dbcac7afd39
aptonic <contact@aptonic.com>
2023-10-05 11:51:21 +1300

    Fix Run Screensaver action under Sonoma

--- Screensaver.dzbundle/action.rb
+++ Screensaver.dzbundle/action.rb
@@ -4,11 +4,11 @@
 # Events: Clicked
 # Creator: Aptonic Software
 # URL: http://aptonic.com
-# Version: 1.0
+# Version: 1.1
 # RunsSandboxed: No
 # MinDropzoneVersion: 3.0
 # UniqueID: 1006
  
 def clicked
-  `osascript -e 'tell application "System Events" to start current screen saver' >& /dev/null`
+  `osascript -e 'tell application "ScreenSaverEngine" to activate' >& /dev/null`
 end
